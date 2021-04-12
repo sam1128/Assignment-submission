6ffdbae134c421e8bf5eac882a3ea655c6431c1e
sam1128 <82404886+sam1128@users.noreply.github.com>
2021-04-12 22:58:22 +0530

    Create Day1.js

---
+++ Day1.js
@@ -0,0 +1,13 @@
+var a= 20;
+var b= 50;
+var c;
+if(a<b){
+console.log("a is less than b");
+c= a+b;
+console.log("a+b=",c);
+}
+else{
+console.log("a is greater than b ");
+c= a-b;
+console.log("a-b=",c);
+}
